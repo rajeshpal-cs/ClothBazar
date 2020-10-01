6f16b16ea13291e89fd7377a84cdc4f1fc7ed125
rajeshpal-cs <rajeshpal.cs@hotmail.com>
2020-10-01 14:52:30 +0530

    Index Created

---
+++ ClothBazar.Services/CategoriesService.cs
@@ -0,0 +1,30 @@
+﻿using ClothBazar.Database;
+using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Services
+{
+   public class CategoriesService
+    {
+        public List<Category> GetCategory()
+        {
+            using (var context = new CBContext())
+            {
+                return context.Categories.ToList();
+            }
+        }
+        public void SaveCategory(Category category)
+        {
+            using(var context= new CBContext())
+            {
+                context.Categories.Add(category);
+                context.SaveChanges();
+            }
+        }
+
+    }
+}
---
+++ ClothBazar.Web/Controllers/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using ClothBazar.Entities;
+using ClothBazar.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClothBazar.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        CategoriesService categoryservice = new CategoriesService();
+        // GET: Category
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var categories = categoryservice.GetCategory();
+            return View(categories);
+        }
+        [HttpGet]
+        public ActionResult Create ()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            categoryservice.SaveCategory(category);
+            return View();
+        }
+    }
+}
