be46ff0af814c547efdb8db841de8aabba7de2ec
rajeshpal-cs <rajeshpal.cs@hotmail.com>
2020-10-14 18:12:47 +0530

    Ajax

---
+++ ClothBazar.Web/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using ClothBazar.Entities;
+using ClothBazar.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClothBazar.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductsService productsService = new ProductsService();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult productTable(string search)
+        {
+            var products = productsService.GetProducts();
+
+            if(!string.IsNullOrEmpty(search))
+            { 
+            products = products.Where(p => p.Name !=null && p.Name.ToLower().Contains( search.ToLower())).ToList();
+            }
+            return PartialView(products);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return PartialView ();
+        }
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            productsService.SaveProduct(product);
+            return RedirectToAction("productTable");
+        }
+    }
+}
